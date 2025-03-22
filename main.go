9386405522f93d6f2380f771795bf7c302c07141
magicznykacpur <kafejek@gmail.com>
2025-03-22 18:00:51 +0100

    refactor main.go

--- main.go
+++ main.go
@@ -35,14 +35,19 @@ func main() {
 
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareServerHitsInc(fileServerHandler))
+
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("GET /api/healthz", handlerHealth)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /admin/users", apiCfg.handlerGetUsers)
+
+	mux.HandleFunc("GET /api/healthz", handlerHealth)
+
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
+
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetAllChirps)
 	mux.HandleFunc("GET /api/chirps/{id}", apiCfg.handlerGetChirpById)
+
 	mux.HandleFunc("POST /api/login", apiCfg.handlerLoginUser)
 
 	server := http.Server{Handler: &mux, Addr: ":" + port}
